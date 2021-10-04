12f396f1c4368979c754136e5e66ba4b083a17f3
Isotarge <isaacjmiell@gmail.com>
2021-10-04 18:37:05 +1030

    Remove some unneeded code

--- src/include/global.h
+++ src/include/global.h
@@ -2,21 +2,7 @@
 
 extern void playSFX(short sfxIndex);
 extern void setPermFlag(short flagIndex);
-extern void openStateMenu(void);
-extern void openTimerSettingsMenu(void);
-extern void openFileStateMainMenu(void);
-extern void openFlagSubmenu(int screenIndex);
-extern void toggleFlag(void);
-extern void handleMapWarping(int map, int levelIndex);
-extern int convertIDToIndex(short obj_index);
+extern int inBadMap(void);
 extern void* findActorWithType(int search_actor_type);
 
 extern void tagAnywhere(void);
-extern void handleTimer(void);
-extern void ramViewUpdate(void);
-extern void displaySavePrompt(void);
-extern void handleAutomoonkick(void);
-extern void handleSpawnPrevention(void);
-extern void controlWatchView(void);
-extern void updateLoadedActorNoTextOverlayList(int callType);
-extern void shouldRefreshTOMenu(void);
