4233c80f680600d0b110b1c1203ef0e9962fe8d3
Isotarge <isaacjmiell@gmail.com>
2021-10-08 00:28:15 +1030

    Simplify

--- build.bat
+++ build.bat
@@ -1,16 +1,16 @@
 @echo off
-mkdir obj
 echo Started: %date% %time%
 
-python .\build\compile.py
+mkdir obj
+python build\compile.py
 move *.o obj\
 
-.\build\armips.exe asm\jump_list.asm
-python .\build\build.py
-.\build\armips.exe asm\main.asm
-del ".\rom\dk64-tag-anywhere-python.z64"
-del ".\rom\dk64-tag-anywhere-temp.z64"
-rmdir /s /q ".\obj"
-.\build\n64crc.exe "rom\dk64-tag-anywhere-dev.z64"
+build\armips.exe asm\jump_list.asm
+python build\build.py
+build\armips.exe asm\main.asm
+del rom\dk64-tag-anywhere-python.z64
+del rom\dk64-tag-anywhere-temp.z64
+rmdir /s /q .\obj
+build\n64crc.exe rom\dk64-tag-anywhere-dev.z64
 echo Completed: %date% %time%
 pause
