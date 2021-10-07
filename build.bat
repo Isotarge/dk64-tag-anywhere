33efe19725067d43e4a8ef32ebeda8df25881698
Isotarge <isaacjmiell@gmail.com>
2021-10-08 01:12:38 +1030

    Remove some unneeded code (you love to see it)

--- build.bat
+++ build.bat
@@ -5,11 +5,9 @@ mkdir obj
 python build\compile.py
 move *.o obj\
 
-build\armips.exe asm\jump_list.asm
 python build\build.py
 build\armips.exe asm\main.asm
 del rom\dk64-tag-anywhere-python.z64
-del rom\dk64-tag-anywhere-temp.z64
 rmdir /s /q .\obj
 build\n64crc.exe rom\dk64-tag-anywhere-dev.z64
 echo Completed: %date% %time%
