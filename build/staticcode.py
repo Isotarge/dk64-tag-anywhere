17663beae52163b6e38766a9868f1a07d2a57bf0
Isotarge <isaacjmiell@gmail.com>
2021-10-09 03:55:24 +1030

    Imagine removing this and it still working... Unbelievable.

--- build/staticcode.py
+++ build/staticcode.py
@@ -4,8 +4,6 @@ StaticCodeFile = "StaticCode.bin"
 
 with open(StaticCodeFile, "r+b") as fh:
 	# RDRAM Address - 0x5FB300 = ROM address
-	fh.seek(0xE64)
-	fh.write(bytearray([0x8,0x0,0x37,0xA2])) # Code Hook: j 0x805DAE20
 	fh.seek(0x15212)
 	fh.write(bytearray([0x80,0x5D])) # Heap Shrink: lui t5, 0x805d
 	# fh.seek(0x119247)
