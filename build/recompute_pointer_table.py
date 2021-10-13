98f3507ea690dd82910d070ba7d8caf1776fcd00
Isaac Miell <isaacjmiell@gmail.com>
2021-10-14 03:52:21 +1030

    Read all pointers

--- build/recompute_pointer_table.py
+++ build/recompute_pointer_table.py
@@ -11,9 +11,11 @@ def dothething(fh : BinaryIO):
   while i < num_tables:
     relative_address = int.from_bytes(fh.read(4), "big")
     pointer_tables.append({
+      "index": i,
       "relative_address": relative_address,
       "absolute_address": relative_address + main_pointer_table_offset,
-      "num_entries": 0
+      "num_entries": 0,
+      "entries": []
     })
     i += 1
 
@@ -25,4 +27,18 @@ def dothething(fh : BinaryIO):
 
   for x in pointer_tables:
     if x["num_entries"] > 0:
+      fh.seek(x["absolute_address"])
+      i = 0
+      while i < x["num_entries"]:
+        relative_address = int.from_bytes(fh.read(4), "big") & 0x7FFFFFFF
+        x["entries"].append({
+          "index": i,
+          "relative_address": relative_address,
+          "absolute_address": relative_address + main_pointer_table_offset,
+          "compressed_size": 0
+        })
+        i += 1
       print(hex(x["absolute_address"]) + ": " + str(x["num_entries"]))
+      if x["num_entries"] < 256:
+        for y in x["entries"]:
+          print(" - " + str(y["index"]) + ": " + hex(y["absolute_address"]) + " (" + hex(y["compressed_size"]) + ")")
