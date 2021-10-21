c611ba84bccc7a9806f54a3d8a03218ffb6d1f14
Isotarge <isaacjmiell@gmail.com>
2021-10-21 22:03:30 +1030

    Support bit set index lookups

--- build/extract_maps.py
+++ build/extract_maps.py
@@ -9,67 +9,56 @@ relevant_pointer_tables = [
         "index": 1,
         "name": "Map Geometry",
         "output_filename": "geometry.bin",
-        "is_compressed": True,
     },
     {
         "index": 2,
         "name": "Map Walls",
         "output_filename": "walls.bin",
-        "is_compressed": True,
     },
     {
         "index": 3,
         "name": "Map Floors",
         "output_filename": "floors.bin",
-        "is_compressed": True,
     },
     {
         "index": 8,
         "name": "Map Cutscenes",
         "output_filename": "cutscenes.bin",
-        "is_compressed": True,
     },
     {
         "index": 9,
         "name": "Map Setups",
         "output_filename": "setup.bin",
-        "is_compressed": True,
     },
     {
         "index": 10,
         "name": "Map Data 0xA",
         "output_filename": "map_0x0a.bin",
-        "is_compressed": True,
     },
     {
         "index": 15,
         "name": "Map Paths",
         "output_filename": "paths.bin",
-        "is_compressed": False,
     },
     {
         "index": 16,
         "name": "Map Paths",
         "output_filename": "character_spawners.bin",
-        "is_compressed": True,
     },
     {
         "index": 18,
         "name": "Map Loading Zones",
         "output_filename": "loading_zones.bin",
-        "is_compressed": True,
     },
     {
         "index": 21,
         "name": "Map Data 0x15",
         "output_filename": "map_0x15.bin",
-        "is_compressed": True,
     },
     {
         "index": 23,
         "name": "Map Exits",
         "output_filename": "exits.bin",
-        "is_compressed": True,
     },
 ]
 
@@ -106,14 +95,20 @@ def extractMap(mapIndex : int, mapPath : str):
             print("Warning: Pointer table " + str(pointer_table["index"]) + " did not contain an entry for index " + str(mapIndex))
             return
 
-        # TODO: Support entry["bit_set"] index lookups
         entry = pointer_tables[pointer_table["index"]]["entries"][mapIndex]
+
+        if entry["bit_set"]:
+            file_info = getFileInfo(entry["absolute_address"])
+            if file_info:
+                index = int.from_bytes(bytes([file_info["data"][0], file_info["data"][1]]), "big")
+                entry = pointer_tables[pointer_table["index"]]["entries"][index]
+
         file_info = getFileInfo(entry["absolute_address"])
         if file_info:
             if len(file_info["data"]) > 0:
                 built_filename = mapPath + pointer_table["output_filename"]
                 data = file_info["data"]
-                if pointer_table["is_compressed"] and len(data) > 2 and data[0] == 0x1F and data[1] == 0x8B:
+                if len(data) > 3 and data[0] == 0x1F and data[1] == 0x8B and data[2] == 0x08:
                     with open(built_filename, "wb") as fh:
                         fh.write(zlib.decompress(data, 15 + 32))
                 else:
